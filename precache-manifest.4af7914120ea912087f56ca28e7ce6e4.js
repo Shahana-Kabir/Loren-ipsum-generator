5fc5b4eca8b8dd09ad1498669faadd2ecaece476
Shahana Kabir <shahana.kabir@gmail.com>
2022-05-11 10:19:45 -0700

    Updates

---
+++ precache-manifest.4af7914120ea912087f56ca28e7ce6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1961f17d5d14e8bffd5ddfd717358f",
+    "url": "/Loren-ipsum-generator/index.html"
+  },
+  {
+    "revision": "bac17e060e105db92998",
+    "url": "/Loren-ipsum-generator/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/Loren-ipsum-generator/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Loren-ipsum-generator/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac17e060e105db92998",
+    "url": "/Loren-ipsum-generator/static/js/main.ef064f79.chunk.js"
+  },
+  {
+    "revision": "44019970c103ac5ac5f6",
+    "url": "/Loren-ipsum-generator/static/js/runtime-main.b231d466.js"
+  }
+]);
